077a134ed91eca9a6e4d258c4c013352e1fc1bb9
megadreams <megadreams14@gmail.com>
2015-11-15 19:23:42 +0900

    多言語対応ファイルの追加と対応方法を記載

--- src/board.js
+++ src/board.js
@@ -1,10 +1,16 @@
+/*
 import {inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-fetch-client';
 import 'fetch';
-
 @inject(HttpClient)
+*/
+
+// 多言語対応
+import {I18N} from 'aurelia-i18n';
+
 export class Board {
-  heading = '掲示板';
+
+//  heading = '掲示板';
 
   ///// 掲示板一覧ページ
   /**
@@ -18,9 +24,41 @@ export class Board {
   */
   boardDetail: {};
 
+  // 多言語対応
+  static inject = [I18N];
 
-  constructor() {
+
+  // TODO: 本来はBaseViewなどを継承し、親Viewで言語設定を行うのが良いと感じている
+  local: 'en';
+  /**
+   * 言語切り替え用のメソッド
+   *
+   */
+  setLocale(local) {
+    this.local = local || 'ja';
+
+    // 言語設定を行う
+    // - ja: 日本語
+    // - en: 英語
+    this.i18n
+        .setLocale(this.local)
+        .then( () => {
+    });
+    // 下記のような形でキーを元に翻訳データを取得する
+    //console.log(this.i18n.tr('boardHeadTitle'));
+  };
+
+  /**
+   * ViewModelが生成されるときに呼ばれるメソッド
+   *  引数に多言語対応用のi18nが呼ばれる
+   */
+  constructor(i18n) {
+    // 多言語対応設定
+    this.i18n = i18n;
+    this.setLocale(this.local);
+
     console.log('constructor');
+    // TODO: ここはhttpでデータを取得したい
     this.boradList = [
       {
         href: '#board/1',
@@ -36,8 +74,7 @@ export class Board {
       }
     ];
 
-
-    // APIを元に取得するデータ
+    // TODO: APIを元に取得するデータ
     this.config = [
       {
         boradId: 1,
@@ -90,21 +127,33 @@ export class Board {
 
   };
 
+  /**
+   * URLパラメータを受け取る方法
+   *  - constructor が呼ばれてからactivateが呼ばれる
+   *  - TODO: renderのタイミングはconstructorでも走るのか？
+   *     - どのタイミングでrenderが走るのかは調べる必要がある
+   */
   activate(params) {
     console.log('activate');
     console.log(params);
     var boardId = (params.id - 1);
-
+    // とりあえず表示を切り替えるために設定してみる
     this.boardDetail = this.config[boardId];
+
   };
 
-  // click.delegateでViewとViewModelのひも付けを行う
-  // - clic.delegate="createBoard()"
+  /**
+  * createBoard Buttonが押された時に呼ばれるメソッド
+  *
+  * ViewModel と View のイベントのひも付けは[click.delegate]を利用している
+  */
   createBoard() {
-    alert('新規作成');
+    alert(this.i18n.tr('boardAddNewButton'));
   };
 
-  // renderのタイミングっていつなのだろう？
+  // TODO: renderのタイミングっていつなのだろう？
+  // TODO: http通信するにはどうしたら良いのだろう？
+
 
   /*
   users = [];
